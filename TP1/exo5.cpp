ea35c4d3bd32b376eca79325367c8047bf341bb1
coralie <coralie.bnnd@gmail.com>
2023-03-13 15:08:20 +0100

    ex5tp1

--- TP1/exo5.cpp
+++ TP1/exo5.cpp
@@ -6,8 +6,18 @@ int isMandelbrot(Point z, int n, Point point){
     // recursiv Mandelbrot
 
     // check n
-
-    // check length of z
+    if(n>0){
+       // check length of z
+        int module = sqrt(z.x*z.x + z.y*z.y);
+        if(module<=2){
+            Point newZ;
+            newZ.x = (z.x*z.x - z.y*z.y) + point.x;
+            newZ.y = (2 * z.x * z.y) + point.y;
+            return isMandelbrot(newZ, n-1, point);
+        }else{
+            return n;
+        }
+    }
     // if Mandelbrot, return 1 or n (check the difference)
     // otherwise, process the square of z and recall
     // isMandebrot
