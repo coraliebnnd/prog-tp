fcfb408c059eb0ab7d82c12e0969befad4fe4a46
coralie <coralie.bnnd@gmail.com>
2023-03-17 10:06:32 +0100

    tp3ex3

--- TP3/exo3.cpp
+++ TP3/exo3.cpp
@@ -65,46 +65,103 @@ struct SearchTreeNode : public Node
         // should return the sum of nodes within left child and
         // right child +1 for itself. If there is no child, return
         // just 1
+
         int sumLeft = 0;
         int sumRight = 0;
 
         if(this->left != nullptr){
            sumLeft = this->left->nodesCount();
-           sumLeft++;
         }
 
         if(this->right != nullptr){
             sumRight = this->right->nodesCount();
-            sumRight++;
         }
 
-        return sumRight+sumLeft;
+        return sumRight+sumLeft+1;
 	}
 
 	bool isLeaf() const {
         // return True if the node is a leaf (it has no children)
-        return false;
+        if(this->left == nullptr && this->right == nullptr){
+            return true;
+        }else{
+            return false;
+        }
 	}
 
 	void allLeaves(Node* leaves[], uint& leavesCount) {
         // fill leaves array with all leaves of this tree
+        if(this->isLeaf()){
+            leaves[leavesCount] = this;
+            leavesCount++;
+        }else{
+            if(this->left != nullptr){
+                this->left->allLeaves(leaves, leavesCount);
+            }
+
+            if(this->right != nullptr){
+                this->right->allLeaves(leaves, leavesCount);
+            }
+        }
 	}
 
 	void inorderTravel(Node* nodes[], uint& nodesCount) {
         // fill nodes array with all nodes with inorder travel
+        if(this->left != nullptr){
+            this->left->inorderTravel(nodes, nodesCount);
+        }
+
+        nodes[nodesCount] = this;
+        nodesCount++;
+
+        if(this->right != nullptr){
+            this->right->inorderTravel(nodes, nodesCount);
+        }
 	}
 
 	void preorderTravel(Node* nodes[], uint& nodesCount) {
         // fill nodes array with all nodes with preorder travel
+        nodes[nodesCount] = this;
+        nodesCount++;
+
+        if(this->left != nullptr){
+            this->left->preorderTravel(nodes, nodesCount);
+        }
+
+        if(this->right != nullptr){
+            this->right->preorderTravel(nodes, nodesCount);
+        }
 	}
 
 	void postorderTravel(Node* nodes[], uint& nodesCount) {
         // fill nodes array with all nodes with postorder travel
+        if(this->left != nullptr){
+            this->left->postorderTravel(nodes, nodesCount);
+        }
+
+        if(this->right != nullptr){
+            this->right->postorderTravel(nodes, nodesCount);
+        }
+
+        nodes[nodesCount] = this;
+        nodesCount++;
 	}
 
 	Node* find(int value) {
         // find the node containing value
-		return nullptr;
+        if(this->value == value){
+            return this;
+        }else{
+            if(this->left != nullptr){
+                return this->left->find(value);
+            }
+
+            if(this->right != nullptr){
+                return this->right->find(value);
+            }
+        }
+
+        return nullptr;
 	}
 
     void reset()
