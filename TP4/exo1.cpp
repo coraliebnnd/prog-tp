e9d30a661f35110961019a9def276e3a38855fbb
coralie <coralie.bnnd@gmail.com>
2023-03-17 12:26:52 +0100

    ex1tp4

--- TP4/exo1.cpp
+++ TP4/exo1.cpp
@@ -11,24 +11,47 @@ using std::string;
 
 int Heap::leftChildIndex(int nodeIndex)
 {
-    return 0;
+    return nodeIndex*2+1;
 }
 
 int Heap::rightChildIndex(int nodeIndex)
 {
-    return 0;
+    return nodeIndex*2+2;
 }
 
 void Heap::insertHeapNode(int heapSize, int value)
 {
 	// use (*this)[i] or this->get(i) to get a value at index i
 	int i = heapSize;
+    this->get(i) = value;
+    while(i>0 && this->get(i)>this->get((i-1)/2)){
+        swap(this->get(i), this->get((i-1)/2));
+        i = (i-1)/2;
+    }
 }
 
 void Heap::heapify(int heapSize, int nodeIndex)
 {
 	// use (*this)[i] or this->get(i) to get a value at index i
-	int i_max = nodeIndex;
+    int i_max = nodeIndex;
+
+    int indexLeft = leftChildIndex(nodeIndex);
+    int indexRight = rightChildIndex(nodeIndex);
+
+    int valueNode = this->get(nodeIndex);
+
+    if(indexLeft < heapSize && this->get(indexLeft) > valueNode){
+         i_max = indexLeft;
+    }
+
+    if(indexRight < heapSize && this->get(indexRight) > valueNode){
+         i_max = indexRight;
+    }
+
+    if(i_max != nodeIndex){
+        std::swap(this->get(nodeIndex), this->get(i_max));
+        this->heapify(heapSize, i_max);
+    }
 }
 
 void Heap::buildHeap(Array& numbers)
